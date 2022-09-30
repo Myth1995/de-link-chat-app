9d5df0510f7a5350829b423c5c5991d75474bfff
Myth1995 <Myth19950803@gmail.com>
2022-09-30 15:23:05 +0900

    Add teams

---
+++ server/routes/chatRoutes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const {
+  accessChat,
+  fetchChats,
+  createTeamChat,
+  renameGroup,
+  addToGroup,
+  removeFromGroup,
+} = require("../controllers/chatControllers");
+const auth = require("../middleware/authMiddleware");
+
+router.route("/").post(auth, accessChat).get(auth, fetchChats);
+
+router.route("/team").post(auth, createTeamChat);
+
+router.route("/grouprename").put(auth, renameGroup);
+
+router.route("/groupremove").put(auth, removeFromGroup);
+
+router.route("/groupadd").put(auth, addToGroup);
+
+module.exports = router;
